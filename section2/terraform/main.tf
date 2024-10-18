351e7bcfdb6df6cbf687da5f2e9bd123b8beb569
weiqichua <143248754+weiqichua@users.noreply.github.com>
2024-10-18 20:32:31 +0800

    trigger workflow

--- section2/terraform/main.tf
+++ section2/terraform/main.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "app_server" {
   instance_type = "t3.small"              # EC2 Instance Type
 
 
-
   associate_public_ip_address = true
   subnet_id                   = var.tf_workshop_ex1_subnet_id
   vpc_security_group_ids      = [var.tf_workshop_ex1_vpc_sg_id]
