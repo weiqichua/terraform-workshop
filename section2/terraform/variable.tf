263e42cfaa71b003753636a81ac0bf505c571081
weiqichua <143248754+weiqichua@users.noreply.github.com>
2024-10-18 20:26:15 +0800

    trigger workflow

--- section2/terraform/variable.tf
+++ section2/terraform/variable.tf
@@ -2,12 +2,12 @@
 # In a real-world scenario, you would want to use a data source to look up the VPC ID.
 variable "tf_workshop_ex1_vpc_sg_id" {
   description = "The ID of the VPC for Exercise 1"
-  default     = ["sg-0f9b9c5a891d88da4"]
+  default     = "sg-0f9b9c5a891d88da4"
 }
 
 variable "tf_workshop_ex1_subnet_id" {
   description = "The ID of the Subnet for Exercise 1"
-  default     = ["subnet-0b9cc866875838ab6"]
+  default     = "subnet-0b9cc866875838ab6"
 }
 
 # This value is taken from the pipeline environment variables
